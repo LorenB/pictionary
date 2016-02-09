d17bb34bacccf1d0be53e5724033a1744379f87d
lorenb <loren.barcus@gmail.com>
2016-02-09 02:03:30 +0000

    Allow only one user to have 'drawer' role. @todo: enforce drawing permissions

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 var pictionary = function() {
     var socket = io();
     var canvas, context;
-    
+    var playerRole = 'guesser';
+    $("#player-role span").text(playerRole);
 
     var draw = function(position) {
         context.beginPath();
@@ -43,6 +44,24 @@ var pictionary = function() {
         // console.log(msg);
         $("#playerguess span").text(msg);
     });
+    
+    var becomeDrawerBtn = $('#become-drawer');
+    becomeDrawerBtn.on('click', function(event) {
+       console.log("you want to be the drawer") ;
+       playerRole = 'drawer';
+       $("#become-drawer").text('');
+       $("#player-role span").text(playerRole);
+       socket.emit('newDrawer', 'yes');
+    });
+    socket.on('newDrawer', function(msg){
+        // console.log(msg);
+        console.log("A user has become the drawer");
+        playerRole = 'guesser';
+        $("#player-role span").text(playerRole);
+        $("#become-drawer").text('Become Drawer');
+        
+    });    
+    
 };
 
 
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 var http = require("http");
 var express = require("express");
 var socket_io = require("socket.io");
+var clients = {};
 
 var app = express();
 app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var i = 0;
 
 io.on('connection', function (socket) {
     console.log('Client connected');
+    // console.log(i);
+    // i += 1;
+    socket.on('disconnect', function() {
+        console.log('This user disconnected:');
+        console.log(socket.id);
+        delete clients[socket.id];
+    });    
+    
+    clients[socket.id] = socket;
+    Object.keys(clients).forEach(function(key) {
+        console.log(key);
+    });    
+    
     socket.on('draw', function(msg) {
         // console.log(msg);
         io.emit('draw', msg);
@@ -17,6 +32,9 @@ io.on('connection', function (socket) {
     socket.on('guess', function(msg) {
         // console.log(msg);
         socket.broadcast.emit('guess', msg);
+    });
+    socket.on('newDrawer', function(msg) {
+        socket.broadcast.emit('newDrawer', msg);
     });
   
 });
